cef6db9496eea0237b43bf54970c9196b322f867
ayushkumar8877 <ayushkumar88770@gmail.com>
2025-07-14 15:58:14 +0530

    initial commit

---
+++ FRONTEND/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    // 👇 yeh line add ki — refresh pe React Router ke path ko handle karega
+    historyApiFallback: true,
+  },
+})
